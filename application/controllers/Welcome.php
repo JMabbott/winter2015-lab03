6dabbc36a0a1629e26656fe1ec588ea612a3862e
JMabbott <joelgwm@gmail.com>
2015-01-25 07:06:48 -0800

    Completed tutorial 3 Comp47ll

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -31,6 +31,16 @@ class Welcome extends Application {
         $this->render();
     }
 
+	function shucks() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        $record = $this->quotes->get(2);
+        
+        $this->data['mug'] = $record['mug'];
+        $this->data['who'] = $record['who'];
+        $this->data['what'] = $record['what'];
+        
+        $this->render();
+    }
 }
 
 /* End of file Welcome.php */
